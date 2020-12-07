f9d4c5024fd6fd005092b6aebbd2afe8ae6d90bd
Sidharth Arya <sidhartharya10@gmail.com>
2020-12-07 08:44:01 +0530

    Auto Commit: Mon Dec  7 08:44:00 AM IST 2020

--- Doom/config.el
+++ Doom/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+;; (setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -53,4 +53,3 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(modules-load '(org))
