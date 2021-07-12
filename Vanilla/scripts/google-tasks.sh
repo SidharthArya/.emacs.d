9f64d4baf995995d6d6521b1c9bf06a0123ef99b
Sidharth Arya <sidhartharya10@gmail.com>
2021-07-12 09:10:18 +0530

    Auto Commit: Mon Jul 12 09:10:18 AM IST 2021

--- Vanilla/scripts/google-tasks.sh
+++ Vanilla/scripts/google-tasks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export PATH="$PATH:/home/arya/.nix-profile/bin/"
-emacs --script ~/.emacs.d/scripts/exercises  &
+emacs --script ~/.emacs.d/scripts/exercises &
 emacs --script ~/.emacs.d/scripts/diet &
 emacs --script ~/.emacs.d/scripts/selfcare &
 
