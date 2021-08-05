ad66e0cc9b30ba79a0502efa788694dc03085afe
Sidharth Arya <sidhartharya10@gmail.com>
2021-08-05 14:19:06 +0530

    Auto Commit: Thu Aug  5 02:19:05 PM IST 2021

--- Vanilla/lisp/projects.el
+++ Vanilla/lisp/projects.el
@@ -6,36 +6,34 @@
 
 
 
-(p! org-projectile
-  :straight t
-  :after org
-  :bind (:map space-prefix
-              ("p O" . org-projectile-project-todo-completing-read)
-              ("p o" . org-projectile-capture-for-current-project)
-              ("p a" . my-org-projectile-agenda)
-              ("p A" . my-org-projectile-agenda-files))
-              
-    :config
-    (setq org-projectile-projects-file "~/Documents/Org/Agenda/projects.org")
-    (org-projectile-per-project)
+;; (p! org-projectile
+;;   :straight t
+;;   :after org
+;;   ("C-c p O" . org-projectile-project-todo-completing-read)
+;;   ("C-c p o" . org-projectile-capture-for-current-project)
+;;   ("C-c p a" . my-org-projectile-agenda)
+;;   ("C-c p A" . my-org-projectile-agenda-files)
+;;     :config
+;;     (setq org-projectile-projects-file "~/Documents/Org/Agenda/projects.org")
+;;     (org-projectile-per-project)
 
-  (defun my-org-projectile-agenda()
-    (interactive)
-    (let*
-        ((org-agenda-files (list org-projectile-projects-file (concat (projectile-project-root) org-projectile-per-project-filepath))))
-      (org-agenda)))
-    (defun my-org-projectile-check-file(a)
-      ""
-       (let* ((file (concat a org-projectile-per-project-filepath)))
-                                                                                    (if (file-exists-p file)
-                                                                                        file)
-                                                                                    ))
-  (defun my-org-projectile-agenda-files()
-    (interactive)
-    (let*
-        ((org-agenda-files (append (list org-projectile-projects-file) (delete nil (mapcar 'my-org-projectile-check-file projectile-known-projects)))))
+;;   (defun my-org-projectile-agenda()
+;;     (interactive)
+;;     (let*
+;;         ((org-agenda-files (list org-projectile-projects-file (concat (projectile-project-root) org-projectile-per-project-filepath))))
+;;       (org-agenda)))
+;;     (defun my-org-projectile-check-file(a)
+;;       ""
+;;        (let* ((file (concat a org-projectile-per-project-filepath)))
+;;                                                                                     (if (file-exists-p file)
+;;                                                                                         file)
+;;                                                                                     ))
+;;   (defun my-org-projectile-agenda-files()
+;;     (interactive)
+;;     (let*
+;;         ((org-agenda-files (append (list org-projectile-projects-file) (delete nil (mapcar 'my-org-projectile-check-file projectile-known-projects)))))
                                                                                        
-                                 (org-agenda)))
+;;                                  (org-agenda)))
 
          
-    (push (org-projectile-project-todo-entry) org-capture-templates))
+;;     (push (org-projectile-project-todo-entry) org-capture-templates))
