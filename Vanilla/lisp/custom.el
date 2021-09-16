ad367604197e4a1822c4976b4ed93e10891d877a
Sidharth Arya <sidhartharya10@gmail.com>
2021-09-16 17:30:01 +0530

    Auto Commit: Thu Sep 16 05:29:59 PM IST 2021

---
+++ Vanilla/lisp/custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
+ '(org-duration-units
+   `(("min" . 1)
+     ("h" . 60)
+     ("d" \,
+      (* 60 8))
+     ("w" \,
+      (* 60 8 5))
+     ("m" \,
+      (* 60 8 5 4))
+     ("y" \,
+      (* 60 8 5 4 12))) nil nil "Customized with use-package org")
+ '(safe-local-variable-values
+   '((org-hugo-section . "braindump")
+     (org-hugo-base-dir . "~/.blog"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
