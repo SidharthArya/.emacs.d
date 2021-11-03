4a4a9a979c1cd73dbb5c4505b0543b4b8575c5dd
Sidharth Arya <sidhartharya10@gmail.com>
2021-11-03 15:38:05 +0530

    Auto Commit: Wed Nov  3 03:38:03 PM IST 2021

--- Vanilla/lisp/gnus.el
+++ Vanilla/lisp/gnus.el
@@ -1,34 +1,34 @@
-(use-package gnus
+(p! gnus
   :straight t
   :custom
-  (gnus-save-newsrc-file . nil)
-  (gnus-home-directory . "~/.news/News/Gnus")
-  (gnus-default-directory . "~/.news/News/Gnus")
-  (gnus-directory . "~/.news/News/Gnus/News")
-  (gnus-agent-directory . "~/.news/News/Gnus/News/agent")
-  (gnus-cache-directory . "~/.news/News/Gnus/News/cache")
-  (gnus-article-save-directory . "~/.news/News/Gnus/News")
-  (gnus-kill-files-directory . "~/.news/News/Gnus/News")
-  (message-directory . "~/.news/News/Gnus/Mail")
-  (nnfolder-directory . "~/.news/News/Gnus/Mail/archive")
-  (gnus-ignored-newsgroups . "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+  (gnus-save-newsrc-file nil)
+  (gnus-home-directory "~/.news/News/Gnus")
+  (gnus-default-directory "~/.news/News/Gnus")
+  (gnus-directory "~/.news/News/Gnus/News")
+  (gnus-agent-directory "~/.news/News/Gnus/News/agent")
+  (gnus-cache-directory "~/.news/News/Gnus/News/cache")
+  (gnus-article-save-directory "~/.news/News/Gnus/News")
+  (gnus-kill-files-directory "~/.news/News/Gnus/News")
+  (message-directory "~/.news/News/Gnus/Mail")
+  (nnfolder-directory "~/.news/News/Gnus/Mail/archive")
+  (gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
                                         ; Adaptive scoring
-  (nnheader-file-name-translation-alist . '((?[ . ?_) (?] . ?_)) )
-  (gnus-select-method .
+  (nnheader-file-name-translation-alist '((?[ . ?_) (?] . ?_)) )
+  (gnus-select-method
    '(nnimap "Personal"
             (nnimap-address "imap.gmail.com")
             (nnimap-server-port "imaps")
             (nnimap-stream ssl))
    (nnir-search-engine imap))
-  (gnus-secondary-select-methods . '(
+  (gnus-secondary-select-methods '(
                                    (nnimap "Gida"
                                            (nnimap-address "imap.gida.in")
                                            (nnimap-server-port "imaps")
                                            (nnimap-stream ssl))
-                                   (nnimap "Startup"
-                                           (nnimap-address "imap.gmail.com")
-                                           (nnimap-server-port "imaps")
-                                           (nnimap-stream ssl))
+                                   ;; (nnimap "Startup"
+                                   ;;         (nnimap-address "imap.gmail.com")
+                                   ;;         (nnimap-server-port "imaps")
+                                   ;;         (nnimap-stream ssl))
                                    ;; (nntp "news.gwene.org")
                                    ))
   :init
@@ -39,7 +39,7 @@
   (gnus-demon-init))
 
 (add-to-list 'kill-emacs-hook 'gnus-group-exit)
-(use-package gnus-desktop-notify
+(p! gnus-desktop-notify
   :config
   (gnus-desktop-notify-mode)
   (gnus-demon-add-scanmail))
