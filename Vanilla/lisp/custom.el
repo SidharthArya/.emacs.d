1c7b213297267c30d9d490542e0ccbbe2b988e43
Sidharth Arya <sidhartharya10@gmail.com>
2022-02-17 10:11:19 +0530

    Auto Commit: Thu Feb 17 10:11:17 AM IST 2022

--- Vanilla/lisp/custom.el
+++ Vanilla/lisp/custom.el
@@ -5,9 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "python Server/manage.py runserver 0.0.0.0:8080")
-     (org-roam-db-location . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database/DB")
-     (org-roam-directory . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database"))))
+   '((projectile-project-run-cmd . "python Server/manage.py runserver 0.0.0.0:8080"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
