0c0363ed1fd5cd7a9b0fd397707320498e9bad3d
Sidharth Arya <sidhartharya10@gmail.com>
2022-02-19 01:32:36 +0530

    Auto Commit: Sat Feb 19 01:32:34 AM IST 2022

--- Vanilla/lisp/custom.el
+++ Vanilla/lisp/custom.el
@@ -4,8 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
+ '(org-duration-units
+   `(("min" . 1)
+     ("h" . 60)
+     ("d" \,
+      (* 60 8))
+     ("w" \,
+      (* 60 8 5))
+     ("m" \,
+      (* 60 8 5 4))
+     ("y" \,
+      (* 60 8 5 4 12))) nil nil "Customized with use-package org")
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "python Server/manage.py runserver 0.0.0.0:8080"))))
+   '((org-roam-db-location . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database/DB")
+     (org-roam-directory . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database")
+     (projectile-project-run-cmd . "python Server/manage.py runserver 0.0.0.0:8080"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
