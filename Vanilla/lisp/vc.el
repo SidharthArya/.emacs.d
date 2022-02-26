dffdc6ebd56fe04bf8ec5352c6b5119072c98208
Sidharth Arya <sidhartharya10@gmail.com>
2022-02-27 02:07:48 +0530

    Auto Commit: Sun Feb 27 02:07:47 AM IST 2022

--- Vanilla/lisp/vc.el
+++ Vanilla/lisp/vc.el
@@ -8,7 +8,7 @@
     (magit-call-git "add" (buffer-file-name))
     (magit-call-git "commit" "-m" (car (cdr (car (org-collect-keywords '("TITLE"))))))
     (magit-run-git-async "push"))
-  (add-to-list 'safe-local-variable-values '(after-save-hook . org-roam--auto-commit)))
+  (add-to-list 'safe-local-variable-values '(after-save-hook . org-roam--auto-commit))
   (setq transient-default-level 5)
   :bind
   ("C-x g" . magit)
