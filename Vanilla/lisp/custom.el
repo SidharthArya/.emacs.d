0efeda478c92ac757785dcfa385e545310640dc9
Sidharth Arya <sidhartharya10@gmail.com>
2022-03-23 09:01:04 +0530

    Auto Commit: Wed Mar 23 09:01:03 AM IST 2022

--- Vanilla/lisp/custom.el
+++ Vanilla/lisp/custom.el
@@ -16,7 +16,9 @@
      ("y" \,
       (* 60 8 5 4 12))) nil nil "Customized with use-package org")
  '(safe-local-variable-values
-   '((org-roam-root-directory . "~/Desktop/")
+   '((org-roam-db-location . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database/DB")
+     (org-roam-directory . "/home/arya/Desktop/Work/DRL/Packaging-Scheduler/making_sense_of_database")
+     (org-roam-root-directory . "~/Desktop/")
      (projectile-project-run-cmd . "python Server/manage.py runserver 0.0.0.0:8080")
      (after-save-hook . org-roam--auto-commit))))
 (custom-set-faces
