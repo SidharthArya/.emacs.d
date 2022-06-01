da10a962194bfdce5745490f5a14f92da13ec1e5
Sidharth Arya <sidhartharya10@gmail.com>
2022-06-02 01:38:52 +0530

    Auto Commit: Thu Jun  2 01:38:52 AM IST 2022

--- Vanilla/lisp/pass.el
+++ Vanilla/lisp/pass.el
@@ -1,16 +1,15 @@
-(use-package pass
-  :defer nil
-  :config
-  (defun org-protocol-pass (fname)
+(p! pass
+  :straight t
+  ;; :config
+  ;; (defun org-protocol-pass (fname)
     ""
-  (let* ((splitparts (org-protocol-parse-parameters fname nil '(:url :type :title :body)))
-         (uri (org-protocol-sanitize-uri (plist-get splitparts :url)))
-         (type (plist-get splitparts :type))
-         (title (plist-get splitparts :title))
-         (body (plist-get splitparts :body))
-         )
-    (when (equal type "query")
+  ;; (let* ((splitparts (org-protocol-parse-parameters fname nil '(:url :type :title :body)))
+  ;;        (uri (org-protocol-sanitize-uri (plist-get splitparts :url)))
+  ;;        (type (plist-get splitparts :type))
+  ;;        (title (plist-get splitparts :title))
+  ;;        (body (plist-get splitparts :body))
+  ;;        )
+    ;; (when (equal type "query")
       
-    )
+    ;; )
   )
-
