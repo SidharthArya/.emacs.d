1b539cc5a8096947977a45237672dd0d206da84c
Sidharth Arya <sidhartharya10@gmail.com>
2022-07-13 09:00:21 +0530

    Auto Commit: Wed Jul 13 09:00:19 AM IST 2022

--- Vanilla/lisp/web.el
+++ Vanilla/lisp/web.el
@@ -2,8 +2,8 @@
   :straight (browse-rules :fetcher github :repo "https://github.com/SidharthArya/browse-rules.el")
   :custom
   (browse-url-browser-function 'browse-rules-url)
-  (browse-rules '(
-                  (".*" t "qutebrowser" "%s")
+  (browse-rules (list
+                  (".*" t (if (equal (system-name) "linux.local") "google-chrome-stable" "qutebrowser") "%s")
                   ;;         (".*\.csv$" t "localc" "%s")
                   ;;                      (".*\.DAT.*" t "localc" "%s")
                   (".*thehindu.*" t "qutebrowser" "https://outline.com/%s")
